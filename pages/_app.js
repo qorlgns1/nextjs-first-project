43513786c20d36ef6842b08fb5607a26323b334b
qorlgns1 <rlgns0610@gmail.com>
2022-08-12 00:07:01 +0900

    Custom App 만들어보기 및 레이아웃패턴 적용
    https://nextjs.org/docs/advanced-features/custom-app

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Layout from '../components/Layout';
 import NavBar from '../components/NavBar';
 import '../styles/globals.css';
 
 export default function App({ Component, ...pageProps }) {
   return (
     <>
-      <NavBar />
-      <Component {...pageProps} />
-      <div>배기훈짱</div>
+      <Layout>
+        <Component {...pageProps} />
+        <div>배기훈짱</div>
+      </Layout>
     </>
   );
 }
